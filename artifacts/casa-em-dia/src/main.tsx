02bb752385b76f1fb23494c7309d3cac6bbfb419
lucaspineda <lucas.pineda@hotmail.com>
2026-04-22 02:40:29 -0300

    ad clarity to project

--- artifacts/casa-em-dia/src/main.tsx
+++ artifacts/casa-em-dia/src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
+import Clarity from "@microsoft/clarity";
 import App from "./App";
 import "./index.css";
+
+Clarity.init("wfivq7r4yy");
 
 createRoot(document.getElementById("root")!).render(<App />);
